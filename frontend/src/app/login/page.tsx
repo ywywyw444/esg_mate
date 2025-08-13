e1e5524756c291a1f74bf7160f4d9aaf50d8ed27
ywywyw444 <vjvm99@gmail.com>
2025-08-13 09:44:14 +0900

    25.08.13 오전

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -42,7 +42,7 @@ export default function LoginPage() {
     
     // 직접 auth-service로 요청 (게이트웨이 우회)
     // const apiUrl = 'https://auth-service-production-f2ef.up.railway.app';
-    const apiUrl = 'https://gateway-production-1104.up.railway.app';
+    const apiUrl = 'https://gateway-production-1104.up.railway.app/api/v1/auth';
                 axios.post(`${apiUrl}/login`, formData)
       .then(response => {
         console.log('Login response:', response.data);
